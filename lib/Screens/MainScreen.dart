33e31728067f5cf75ff5b858d9edfdee69f25d8b
Alex Nussbaumer <alex.nssbmr@gmail.com>
2021-12-04 17:36:25 +0100

    pageview replacing indexed stack

--- lib/Screens/MainScreen.dart
+++ lib/Screens/MainScreen.dart
@@ -2,17 +2,33 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:personal_recipes/Screens/AddRecipeScreen.dart';
+import 'package:personal_recipes/Screens/RecipesScreen.dart';
 import 'package:personal_recipes/Services/GeneralServices.dart';
 import 'package:provider/provider.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final PageController _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     final model = Provider.of<GeneralServices>(context);
     return Scaffold(
-      body: IndexedStack(index: model.index, children: model.screens),
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: PageView(
+        controller: _pageController,
+        children: const [RecipesScreen(), AddRecipeScreen()],
+        onPageChanged: (val) {
+          WidgetsBinding.instance!.focusManager.primaryFocus?.unfocus();
+          model.setIndex(val);
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Theme.of(context).backgroundColor,
         showSelectedLabels: false,
@@ -20,19 +36,22 @@ class MainScreen extends StatelessWidget {
         currentIndex: model.index,
         onTap: (val) {
           WidgetsBinding.instance!.focusManager.primaryFocus?.unfocus();
-          model.setIndex(val);
+          _pageController.animateToPage(val,
+              duration: const Duration(milliseconds: 200),
+              curve: Curves.easeInOut);
         },
         items: [
           BottomNavigationBarItem(
             label: '',
             tooltip: '',
             icon: Center(
-                child: Icon(
-                    Platform.isIOS ? CupertinoIcons.book : Icons.menu_book,
-                    color: model.index == 0
-                        ? Theme.of(context).primaryColor
-                        : Theme.of(context).primaryColor.withOpacity(0.5),
-                    size: model.index == 0 ? 24 : 18)),
+              child: Icon(
+                  Platform.isIOS ? CupertinoIcons.book : Icons.menu_book,
+                  color: model.index == 0
+                      ? Theme.of(context).primaryColor
+                      : Theme.of(context).primaryColor.withOpacity(0.5),
+                  size: model.index == 0 ? 24 : 18),
+            ),
           ),
           BottomNavigationBarItem(
             label: '',
